6bcd315a10adff0cb8c5b3e9efa5429f6099fc2e
SeijiNumata <seizi.numata@gmail.com>
2021-02-11 22:57:23 +0900

    不要な行があったため削除しました

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 #! /usr/bin/env node
 
-require('path')
 const fs = require('fs');
 let currentWorkingDirectory = process.cwd();
 let text = fs.readFileSync(`${currentWorkingDirectory}/html/index.html`,"utf-8");
